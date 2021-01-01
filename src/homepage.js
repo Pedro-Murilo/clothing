30fabc7cdba70ec118c2ce4332bcbb704cbda4c9
Pedro Murilo <pedromurilocalvi@gmail.com>
2021-01-01 01:35:13 -0300

    create the project with create react app and add homepage files

---
+++ src/homepage.js
@@ -0,0 +1,38 @@
+const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Adventure</h1>
+                    <span className="subtitle">Shop now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Action</h1>
+                    <span className="subtitle">Shop now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Indie</h1>
+                    <span className="subtitle">Shop now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">2D</h1>
+                    <span className="subtitle">Shop now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Fantasy</h1>
+                    <span className="subtitle">Shop now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
