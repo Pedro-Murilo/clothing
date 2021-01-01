1246c67d1a362f63c979ead691a4f123d7794e18
Pedro Murilo <pedromurilocalvi@gmail.com>
2021-01-01 15:57:13 -0300

    Broken homepage into smaller components and finished styling menu-item

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
 import './App.css';
-import HomePage  from './homepage.js';
-import './homepage.scss';
+import HomePage  from './pages/homepage/homepage.js';
 
 function App() {
   return (
---
+++ src/pages/homepage/homepage.js
@@ -0,0 +1,10 @@
+import './homepage.scss';
+import Directory from '../../components/directory/directory.js'
+
+const HomePage = () => (
+    <div className="homepage">
+        <Directory />
+    </div>
+)
+
+export default HomePage;
