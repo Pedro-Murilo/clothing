e4ff4c59520872171250e508f4bd9da030361cce
Pedro Murilo <pedromurilocalvi@gmail.com>
2021-01-05 12:47:06 -0300

    Created user-selectors and added createStructuredSelector in cart-selector, header, cart-icon, app.js and cart-dropdown

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import "./App.css";
 import HomePage from "./pages/homepage/homepage";
@@ -10,6 +11,7 @@ import Header from './components/header/header';
 
 import { auth, createUserProfileDocument } from './firebase/firebase';
 import { setCurrentUser } from './redux/user/user-actions';
+import { selectCurrentUser } from './redux/user/user-selectors';
 
 class App extends React.Component {
 
@@ -52,8 +54,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import { auth } from "../../firebase/firebase";
 
@@ -8,6 +9,8 @@ import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 import CartIcon from '../cart-icon/cart-icon';
 import CartDropdown from '../cart-dropdown/cart-dropdown';
+import { selectCartHidden } from '../../redux/cart/cart-selectors';
+import { selectCurrentUser } from '../../redux/user/user-selectors';
 import "./header.scss";
 
 
@@ -40,9 +43,9 @@ const Header = ({ currentUser, hidden }) => (
   </div>
 );
 
-const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
-  currentUser, 
-  hidden
+const mapStateToProps =  createStructuredSelector({
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden
 })
 
 export default connect(mapStateToProps)(Header);
