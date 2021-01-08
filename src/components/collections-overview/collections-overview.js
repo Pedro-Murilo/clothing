1a44e6d3a5a15ba92a69a41a50d41655fc5770fb
Pedro Murilo <pedromurilocalvi@gmail.com>
2021-01-07 21:25:33 -0300

    Moved shopData to a shop redux, create shop-reducer, shop-selector and create CollectionsOverview component

---
+++ src/components/collections-overview/collections-overview.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import  PreviewCollection  from '../preview-collection/preview-collection';
+import { selectCollections } from '../../redux/shop/shop-selectors';
+
+import './collections-overview.scss';
+
+const CollectionsOverview = ({ collections }) => (
+    <div className="collections-overview">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+      <PreviewCollection key={id} {...otherCollectionProps} />
+    ))}
+    </div>
+)
+
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+})
+
+export default connect(mapStateToProps)(CollectionsOverview)
--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -1,29 +1,12 @@
-import React from 'react';
+import React from "react";
 
-import SHOP_DATA from './shopData.js'
-import PreviewCollection from '../../components/preview-collection/preview-collection.js';
+import CollectionsOverview from '../../components/collections-overview/collections-overview'
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
+const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+    <CollectionsOverview />
+  </div>
+);
 
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render() {
-        const { collections } = this.state;
-        return (
-            <div className="shop-page">
-                {
-                    collections.map(({ id, ...otherCollectionProps}) => (
-                        <PreviewCollection key={id} {...otherCollectionProps}/>
-                    ))
-                }
-            </div>
-        )
-    }
-}
 
 export default ShopPage;
