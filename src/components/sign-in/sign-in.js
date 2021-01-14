c70868e9155eed588f0f5e0a88ee32c3c4f1f13c
Pedro Murilo <pedromurilocalvi@gmail.com>
2021-01-14 17:36:02 -0300

    Changed sign-in to styled components

--- src/components/sign-in/sign-in.js
+++ src/components/sign-in/sign-in.js
@@ -5,12 +5,12 @@ import CustomButton from '../../components/custom-button/custom-button.js'
 
 import { auth, signInWithGoogle } from '../../firebase/firebase.js';
 
-import "./sign-in.scss";
+import { SignInContainer, SignInTitle, ButtonsBarContainer } from './sign-in-styles';
 
 class SignIn extends React.Component {
   constructor(props) {
     super(props);
-
+    
     this.state = {
       email: "",
       password: "",
@@ -39,8 +39,8 @@ class SignIn extends React.Component {
 
   render() {
     return (
-        <div className='sign-in'>
-        <h2>I already have an account</h2>
+        <SignInContainer>
+        <SignInTitle>I already have an account</SignInTitle>
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
@@ -60,12 +60,12 @@ class SignIn extends React.Component {
             label='password'
             required
           />
-          <div className="buttons">
+          <ButtonsBarContainer>
             <CustomButton type='submit'> Sign in </CustomButton>
             <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn> Sign in with google </CustomButton>
-          </div>
+          </ButtonsBarContainer>
         </form>
-      </div>
+      </SignInContainer>
     );
   }
 }
